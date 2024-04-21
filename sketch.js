84a56f0a975280cbf6fe00d5b5f962b9a599c14e
fieryone <128168941+fieryone99@users.noreply.github.com>
2024-04-21 12:57:05 +0200

    added sliders for factors

--- sketch.js
+++ sketch.js
@@ -8,8 +8,16 @@ let maxSpeed = 4;
 let minSpeed = 2;
 let size = 7;
 let boids = [];
+
+let alignSlider, cohesionSlider, separationSlider;
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+  alignSlider = createSlider(0, 2, 0.5, 0.1);
+  alignSlider.position(0, 0);
+  cohesionSlider = createSlider(0, 2, 0.35, 0.1);
+  cohesionSlider.position(140, 0);
+  separationSlider = createSlider(0, 2, 0.4, 0.1);
+  separationSlider.position(280, 0);
+  createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100, 100);
     for (let i = 0; i < boidNum; i++) {
       let randomPos = createVector(random(width),                   random(height));
