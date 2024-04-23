290fc785de593cdd3ed19bae6718835fc9fc94a7
fieryone <128168941+fieryone99@users.noreply.github.com>
2024-04-23 17:17:02 +0200

    added the missing this.

--- boid.js
+++ boid.js
@@ -61,7 +61,7 @@ class Boid {
   alignment() {
     let steering = new createVector();
     let total = 0;
-    for (let other of localBoids) {
+    for (let other of this.localBoids) {
       if(this.flockID==other.flockID) {
         steering.add(other.velocity);
         total++;
@@ -79,7 +79,7 @@ class Boid {
   cohesion() {
     let steering = new createVector();
     let total = 0;
-    for (let other of localBoids) {
+    for (let other of this.localBoids) {
       if(this.flockID == other.flockID){
         steering.add(other.position);
         total++;
@@ -98,7 +98,7 @@ class Boid {
   seperation() {
    let steering = createVector();
     let total = 0;
-    for (let other of localBoids) {
+    for (let other of this.localBoids) {
       let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
       if (other != this && d < perceptionRadius) {
         let diff = p5.Vector.sub(this.position,                       other.position);
