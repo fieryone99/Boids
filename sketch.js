e45c52f9444b8576dca17dbf905c3c2f9d9cde21
fieryone <128168941+fieryone99@users.noreply.github.com>
2024-05-05 15:20:11 +0200

    added regenerate button

--- sketch.js
+++ sketch.js
@@ -9,30 +9,25 @@ let size = 7;
 let boids = [];
 
 let alignSlider, cohesionSlider, separationSlider;
+let regenerateButton;
 function setup() {
-  flockNum = floor(random(7));
   alignSlider = createSlider(0, 2, 0.7, 0.1);
   alignSlider.position(0, 0);
   cohesionSlider = createSlider(0, 2, 0.57, 0.1);
   cohesionSlider.position(140, 0);
   separationSlider = createSlider(0, 2, 0.6, 0.1);
   separationSlider.position(280, 0);
+  regenerateButton = createButton('regnerate boids');
+  regenerateButton.position(windowWidth - 100, 10);
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100, 100);
-    for (let i = 0; i < boidNum; i++) {
-      let randomPos = createVector(random(width),                   random(height));
-      let randomAcc = p5.Vector.random2D()
-      .setMag(random(6));
-      let flockID = floor(random(flockNum));
-      let newBoid = new Boid(randomPos, randomAcc,flockID);
-      newBoid.boidNum = boidNum;
-      boids.push(newBoid); 
-    }
+  randomBoids();
 }
 
 function draw() {
   background(0);
   noStroke();
+  regenerateButton.mousePressed(randomBoids);
   alignmentFactor = alignSlider.value();
   cohesionFactor = cohesionSlider.value();
   seperationFactor = separationSlider.value();
@@ -40,4 +35,18 @@ function draw() {
     boids[b].show();
     boids[b].update();
   }
+}
+
+randomBoids() {
+  boids.length = 0;
+  flockNum = floor(random(7));
+for (let i = 0; i < boidNum; i++) {
+      let randomPos = createVector(random(width), random(height));
+      let randomAcc = p5.Vector.random2D()
+      .setMag(random(6));
+      let flockID = floor(random(flockNum));
+      let newBoid = new Boid(randomPos, randomAcc,flockID);
+      newBoid.boidNum = boidNum;
+      boids.push(newBoid); 
+    }
 }
